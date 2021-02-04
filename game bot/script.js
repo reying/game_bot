93a8361edc534d9de215a9be726b42dab392ca29
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-02-04 21:59:53 +0500

    Выполнил усложненное задание урока 6

--- game bot/script.js
+++ game bot/script.js
@@ -1,57 +1,79 @@
 'use strict';
 
-let playerNum = +prompt('Угадай число от 1 до 100:');
+const game = function() {
+    let playerNum = +prompt('Угадай число от 1 до 100:');
 
-// Ф-я проверки на числовой тип
-const isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-const createCounter = function() {
-    let counter = 10;
-    const count = function() {
-        counter = counter - 1;
-        return counter;
+    // Загадываем число - рандомное
+    const getRandom = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    return count;
-};
-const counts = createCounter();
+    const secretNum = getRandom(1, 100);
 
+    // Проверка на принадлежность числу
+    const isNumber = function(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
 
-const checkData = function() {
-    const secretNum = 15;
-    let attempts;
+    // Счетчик попыток
+    const createCounter = function() {
+        let counter = 10;
+        const counts = function() {
+            counter = counter - 1;
+            return counter;
+        };
+        return counts;
+    };
+    const count = createCounter();
 
+    // Проверка введенного значения
+    const checkData = function() {
+        let attempts; //Переменная для хранения счетчика шагов
 
+        // Сравнения чисел бота и игрока
+        const comparisonNum = function() {
+            // Отчлеживание конца игры по кол-ву попыток
+            const endGeme = function() {
+                if (attempts < 1) {
+                    const loser = confirm('Попытки закончились, хотите сыграть еще?');
+                    if (loser) {
+                        game();
+                    } else {
+                        alert('Игра окончена');
+                    }
+                } else {
+                    playerNum = +prompt('Попробуй снова:');
+                    checkData();
+                }
+            };
 
+            // Сравнение чисел 
+            if (playerNum === 0) {
+                alert('Игра окончена');
+            } else if (playerNum > secretNum) {
+                alert('Загаданное число меньше, осталось попыток ' + attempts);
+                endGeme();
+            } else if (playerNum < secretNum) {
+                alert('Загаданное число больше, осталось попыток ' + attempts);
+                endGeme();
+            } else if (playerNum === secretNum) {
+                const winer = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+                if (winer) {
+                    game();
+                } else {
+                    alert('Игра окончена');
+                }
+            }
+        };
 
-    const comparisonNum = function() {
-        if (playerNum === 0) {
-            alert('Игра окончена');
-        } else if (playerNum > secretNum) {
-            alert('Загаданное число меньше, осталось попыток ' + attempts);
+        // Проверка введенного значения
+        if (isNumber(playerNum)) {
+            attempts = count();
+            comparisonNum();
+        } else {
+            alert('Введи число!');
             playerNum = +prompt('Попробуй снова:');
-            checkData();
-        } else if (playerNum < secretNum) {
-            alert('Загаданное число больше, осталось попыток ' + attempts);
-            playerNum = +prompt('Попробуй снова:');
-            checkData();
-        } else if (playerNum === secretNum) {
-            alert('Поздравляю, Вы угадали!!!');
         }
-        // return attempts;
-        // console.log(attempts);
     };
-    if (isNumber(playerNum)) {
-        attempts = counts();
-        comparisonNum();
-    } else {
-        alert('Введи число!');
-        playerNum = +prompt('Попробуй снова:');
-    }
-
+    checkData();
 };
-
-// console.log();
-
-checkData();
+game();

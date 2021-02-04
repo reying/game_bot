5376b337f3b6b646868d63d7bceb9363788ee94c
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-02-04 17:50:52 +0500

    Выполнил основное задание задание урока 6

--- game bot/lesson06/script.js
+++ game bot/lesson06/script.js
@@ -1,23 +1,35 @@
-// Задание 1
-const arr = ['11', '22', '33', '44', '55', '66', '77'];
+'use strict';
 
-for (let i = 0; i < arr.length; i++) {
-    let elemArr = arr[i].charAt(0);
+const secretNum = 10;
+let playerNum = +prompt('Угадай число от 1 до 100:');
 
-    if (elemArr === '2' || elemArr === '4') {
-        console.log(arr[i]);
-    }
-}
+// Ф-я проверки на числовой тип
+const isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
-// Задание 2
-nextStep:
-    for (let i = 1; i < 101; i++) {
-        const result = 'Делители этого числа: 1 и ' + i;
-        if (i === 1) { continue nextStep; }
-        for (let j = 2; j < i; j++) {
-            if (i % j === 0) {
-                continue nextStep;
-            }
+const checkData = function() {
+    const comparisonNum = function() {
+        if (playerNum === 0) {
+            alert('Игра окончена');
+        } else if (playerNum > secretNum) {
+            alert('Загаданное число меньше!');
+            playerNum = +prompt('Попробуй снова:');
+            checkData();
+        } else if (playerNum < secretNum) {
+            alert('Загаданное число больше!');
+            playerNum = +prompt('Попробуй снова:');
+            checkData();
+        } else if (playerNum === secretNum) {
+            alert('Поздравляю, Вы угадали!!!');
         }
-        console.log(result);
+    };
+
+    if (isNumber(playerNum)) {
+        comparisonNum();
+    } else {
+        alert('Введи число!');
+        playerNum = +prompt('Попробуй снова:');
     }
+};
+checkData();

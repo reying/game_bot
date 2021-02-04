1eb142a3cc0be6a4f5f538970fd7c094df7ef7b2
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-02-04 20:18:06 +0500

    save1

--- game bot/script.js
+++ game bot/script.js
@@ -7,29 +7,51 @@ const isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+const createCounter = function() {
+    let counter = 10;
+    const count = function() {
+        counter = counter - 1;
+        return counter;
+    };
+    return count;
+};
+const counts = createCounter();
+
+
 const checkData = function() {
-    const secretNum = 10;
+    const secretNum = 15;
+    let attempts;
+
+
+
+
     const comparisonNum = function() {
         if (playerNum === 0) {
             alert('Игра окончена');
         } else if (playerNum > secretNum) {
-            alert('Загаданное число меньше!');
+            alert('Загаданное число меньше, осталось попыток ' + attempts);
             playerNum = +prompt('Попробуй снова:');
             checkData();
         } else if (playerNum < secretNum) {
-            alert('Загаданное число больше!');
+            alert('Загаданное число больше, осталось попыток ' + attempts);
             playerNum = +prompt('Попробуй снова:');
             checkData();
         } else if (playerNum === secretNum) {
             alert('Поздравляю, Вы угадали!!!');
         }
+        // return attempts;
+        // console.log(attempts);
     };
-
     if (isNumber(playerNum)) {
+        attempts = counts();
         comparisonNum();
     } else {
         alert('Введи число!');
         playerNum = +prompt('Попробуй снова:');
     }
+
 };
+
+// console.log();
+
 checkData();
